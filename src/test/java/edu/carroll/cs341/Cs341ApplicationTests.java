600f5ccd8e9150330d3a2196875262304be2e12a
Molly O'Connor <moconnor@carroll.edu>
2024-08-21 14:56:19 -0600

    Initial commit

---
+++ src/test/java/edu/carroll/cs341/Cs341ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.carroll.cs341;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs341ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
