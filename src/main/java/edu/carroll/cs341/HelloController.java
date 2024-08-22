111c273391c516535fe4505e936903624a81d271
Molly O'Connor <moconnor@carroll.edu>
2024-08-21 19:43:27 -0600

    Fix HTML formatting

--- src/main/java/edu/carroll/cs341/HelloController.java
+++ src/main/java/edu/carroll/cs341/HelloController.java
@@ -1,14 +1,14 @@
 package edu.carroll.cs341;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
     @GetMapping("/")
-    public String index(@RequestParam(value = "name", required = false, defaultValue = "Student") String name, Model model) {
+    public String index(@RequestParam(value="name", required=false, defaultValue="Student")String name, Model model) {
         model.addAttribute("name", name);
         return "index";
     }
