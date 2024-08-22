7a722617984dfbf4db69109e2043ef90b8f7213b
Molly O'Connor <moconnor@carroll.edu>
2024-08-21 19:32:39 -0600

    Render HTML using content from Java Code

--- src/main/java/edu/carroll/cs341/HelloController.java
+++ src/main/java/edu/carroll/cs341/HelloController.java
@@ -2,11 +2,14 @@ package edu.carroll.cs341;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String index() {
+    @GetMapping("/")
+    public String index(@RequestParam(value = "name", required = false, defaultValue = "Student") String name, Model model) {
+        model.addAttribute("name", name);
         return "index";
     }
 }
