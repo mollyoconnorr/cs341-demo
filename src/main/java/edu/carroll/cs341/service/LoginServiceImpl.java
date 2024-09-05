fdd6d0e73d74016b61c919bbcf4eaa326ed43004
Molly O'Connor <moconnor@carroll.edu>
2024-09-05 10:35:02 -0600

    Application Logging

--- src/main/java/edu/carroll/cs341/service/LoginServiceImpl.java
+++ src/main/java/edu/carroll/cs341/service/LoginServiceImpl.java
@@ -4,9 +4,13 @@ import edu.carroll.cs341.jpa.model.Login;
 import edu.carroll.cs341.jpa.repo.LoginRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class LoginServiceImpl implements LoginService {
+    private static final Logger log = LoggerFactory.getLogger(LoginServiceImpl.class);
+
     private final LoginRepository loginRepo;
 
     public LoginServiceImpl(LoginRepository loginRepo) {
@@ -22,11 +26,14 @@ public class LoginServiceImpl implements LoginService {
      */
     @Override
     public boolean validateUser(String username, String password) {
+        log.debug("validateUser: user '{}' attempted login", username);
+
         // Always do the lookup in a case-insensitive manner (lower-casing the data).
         List<Login> users = loginRepo.findByUsernameIgnoreCase(username);
 
         // We expect 0 or 1, so if we get more than 1, bail out as this is an error we don't deal with properly.
         if (users.size() != 1) {
+            log.debug("validateUser: found {} users", users.size());
             return false;
         }
 
@@ -38,10 +45,12 @@ public class LoginServiceImpl implements LoginService {
 
         // Check if the provided password hash matches the stored hashed password
         if (!u.getHashedPassword().equals(userProvidedHash)) {
+            log.debug("validateUser: password does not match for user '{}'", username);
             return false;
         }
 
         // User exists, and the provided password matches the hashed password in the database.
+        log.info("validateUser: successful login for user '{}'", username);
         return true;
     }
 }
